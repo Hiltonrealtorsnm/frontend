f6abcdde829e52d366cff413550f2a0a3ea598d5
Arunarasan <arunarasan06@gmail.com>
2025-12-07 18:05:53 +0530

    rendering

--- realestate_frontend/src/App.js
+++ realestate_frontend/src/App.js
@@ -81,7 +81,7 @@ export default function App() {
 
           <Route
             path="/admin/dashboard"
-            element={<AdminProtected><AdminDashboard /></AdminProtected>}
+            element={<AdminDashboard />}
           />
 
           <Route
